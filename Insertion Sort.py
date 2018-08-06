675a74f6d61557e04434941598a7190a24bdedef
Brent Porter <eporter2@live.maryville.edu>
2018-08-05 19:57:35 -0600

    selection sort and insertion sort complete

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -2,4 +2,25 @@
 # GitHub ID: eporter2maryville
 # Assignment: 5.1.3 Insertion Sort
 
-testIntegers = [ 1,2,3,4,5,6,7,8,9,10 ]
+testIntegers = [ 10,2,5,4,1,3,7,9,8,6 ]
+
+def insertionSort(list):
+    n = len(list)
+    #loop to run it the correct number of passes and assigns the first item to the new list
+    for index in range(1,n):
+        currentValue = list[index]
+        position = index
+        #compares each successive item to the current list and places it appropriately
+        while position>0 and list[position-1]>currentValue:
+            list[position] = list[position-1]
+            position = position-1
+            
+        list[position]=currentValue
+    
+def main():
+    print(testIntegers) #original jumbled list
+    insertionSort(testIntegers) #performing sort
+    print(testIntegers) #sorted list
+    
+    
+main()
--- Selection Sort.py
+++ Selection Sort.py
@@ -2,4 +2,30 @@
 # GitHub ID: eporter2maryville
 # Assignment: 5.1.2 Selection Sort
 
-testIntegers = [ 1,2,3,4,5,6,7,8,9,10 ]
+testIntegers = [ 10,2,5,4,1,3,7,9,8,6 ]
+
+#The selection sort identifies the number that should be at the end of the list and then places it there.
+#Each successive pass builds the list by placing the next to last number until all numbers are in place.
+
+def selectionSort(list):
+    n = len(list)
+    
+    #determines number of passes
+    for fillslot in range (n-1, 0, -1):
+        positionOfMax=0
+        #iterates through list
+        for location in range (1, fillslot+1):
+            if list[location]>list[positionOfMax]:
+                positionOfMax = location
+        #places selected number
+        temp = list[fillslot]
+        list[fillslot] = list[positionOfMax]
+        list[positionOfMax] = temp
+
+def main():
+    print(testIntegers) #original jumbled list
+    selectionSort(testIntegers) #performing sort
+    print(testIntegers) #sorted list
+    
+    
+main()
