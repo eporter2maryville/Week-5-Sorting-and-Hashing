5528dc1d87b58cb40c98a10826f8e45e6a1a775d
Brent Porter <eporter2@live.maryville.edu>
2018-08-05 19:01:06 -0600

    Initial commit

---
+++ Bubble Sort.py
@@ -0,0 +1,3 @@
+# Name: Brent Porter
+# GitHub ID: eporter2maryville
+# Assignment: 5.1.1 Bubble Sort
---
+++ Selection Sort.py
@@ -0,0 +1,3 @@
+# Name: Brent Porter
+# GitHub ID: eporter2maryville
+# Assignment: 5.1.2 Selection Sort
